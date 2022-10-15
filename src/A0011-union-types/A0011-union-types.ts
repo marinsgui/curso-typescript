5d6ec9628677ea7cde1b6f118f0dc4fab153000e
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2022-10-15 10:40:04 -0300

    Union types

---
+++ src/A0011-union-types/A0011-union-types.ts
@@ -0,0 +1,11 @@
+function addOrConcat(a: number | string | boolean, b: number | string | boolean): number | string {
+  if(typeof a === 'number' && typeof b === 'number') return a + b;
+  return `${a}${b}`;
+}
+
+console.log(addOrConcat(10, 20));
+console.log(addOrConcat(10, '20'));
+console.log(addOrConcat('10', 20));
+console.log(addOrConcat(10, true));
+console.log(addOrConcat('10', true));
+console.log(addOrConcat(true, false));
