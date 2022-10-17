4768a4bde9cc5959ad2da5cae7f9c03baa3b22a6
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2022-10-17 10:53:49 -0300

    Classes em typescript

---
+++ src/A0021-classes/A0021-classes.ts
@@ -0,0 +1,37 @@
+export class Empresa {
+  public readonly nome: string;
+  private readonly colaboradores: Colaborador[] = [];
+  protected readonly cnpj: string;
+
+  constructor(nome: string, cnpj: string) {
+    this.nome = nome;
+    this.cnpj = cnpj;
+  }
+
+  adicionaColaborador(colaborador: Colaborador): void {
+    this.colaboradores.push(colaborador);
+  }
+
+  mostraColaborador(): void {
+    for(const colaborador of this.colaboradores) {
+      console.log(colaborador);
+    }
+  }
+}
+
+export class Colaborador {
+  constructor(
+    public readonly nome: string,
+    public readonly sobrenome: string
+    ) {}
+}
+
+const empresa1 = new Empresa('Udemy', '11.111.111/1111-11');
+const colaborador1 = new Colaborador('Guilherme', 'Marins');
+const colaborador2 = new Colaborador('João', 'Silva');
+const colaborador3 = new Colaborador('Maria', 'Júlia');
+empresa1.adicionaColaborador(colaborador1);
+empresa1.adicionaColaborador(colaborador2);
+empresa1.adicionaColaborador(colaborador3);
+console.log(empresa1);
+empresa1.mostraColaborador();
